7ddf2196318da0a73737645d299f88e8d37ad7ed
sarthak singhal <sarthak.singhal@students.iiit.ac.in>
2020-05-09 10:42:19 +0530

    percentage analysis added

--- code/code.py
+++ code/code.py
@@ -65,15 +65,43 @@ def part1():
         t1[t1>0]=1#water limited
         T="Water-Energy Zones For Year "+str(1951+i)
         plot_hm(t1,T)
-    pass
+
+
+
+def part2():
+    """
+    Expansion/Reduction
+    """
+
+    MPET,MR=monthly()
+    E,W,Y=[],[],[]
+    for i in range(64):
+        t1=MPET[:,:,i]-MR[:,:,i]
+        t1[t1<0]=-1#energy limited
+        t1[t1>0]=1#water limited
+        val,freq=np.unique(t1,return_counts=True)        
+        # print(np.asarray((val,freq)))
+        # print(freq[0])
+        E.append(freq[0]/(freq[0]+freq[2]))
+        W.append(freq[2]/(freq[0]+freq[2]))
+        Y.append(1951+i)
+
+    plt.subplot(211)
+    plt.title("Percentage of Zones")
+    plt.plot(Y,E,'y',label='Energy limited')
+    plt.legend()
+    
+    plt.subplot(212)
+    plt.plot(Y,W,'b',label='Water limited')
+    plt.legend()
+    plt.show()
 
 
 
 if __name__ == "__main__":
     
     PET,R=load_data()
-    print(PET.shape)
-    print(R.shape)
     # (121, 121, 768) 64 years monthly data
 
-    part1()
+    # part1()
+    part2()
