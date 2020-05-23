803dff7a06ec9638bf5cb83efbe5bc65e944d8cc
sarthak singhal <sarthak.singhal@students.iiit.ac.in>
2020-05-23 11:29:41 +0530

    more analytics added

--- code/code.py
+++ code/code.py
@@ -77,7 +77,7 @@ def zone_mask(zone):
 
 
 
-def plot_hm(X,T):
+def plot_hm(X,T,zone=0):
     """
     Plot heat map
     """
@@ -85,13 +85,15 @@ def plot_hm(X,T):
     X=X.transpose()
     X=np.flipud(X)
     # mask=get_mask()#for changing grid points
-    mask=zone_mask(7)#for individual zones
-    ax=sns.heatmap(X,xticklabels=False,yticklabels=False,cmap="YlGnBu",cbar=False,mask=mask)
+    mask=zone_mask(zone)#for individual zones
+    # ax=sns.heatmap(X,xticklabels=False,yticklabels=False,cmap="YlGnBu",cbar=False,mask=mask)
+    ax=sns.heatmap(X,xticklabels=False,yticklabels=False,cmap="YlGnBu",mask=mask)
     # ax=sns.heatmap(X,xticklabels=False,yticklabels=False,cmap="YlGnBu",cbar=False)
     
     plt.title(T)
     # plt.show()
-    plt.savefig("images/zone/7/"+T+".png")
+    plt.savefig("images/zonal_analysis/"+str(zone)+"/"+T+".png")
+    plt.close()
 
 
 
@@ -117,7 +119,7 @@ def yearly():
 
 def part1():
     """
-    Yearly zone variation
+    Yearly zone variation (64 plots each)
     """
 
     MPET,MR=yearly()
@@ -132,7 +134,7 @@ def part1():
 
 def part2():
     """
-    Expansion/Reduction
+    Global Expansion/Reduction
     """
 
     MPET,MR=yearly()
@@ -294,8 +296,8 @@ def zone_analysis(data):
     7 = north zone
     """
 
+    global T
     colors = cm.rainbow(np.linspace(0,1,8)) 
-    T={1:"Central Zone",2:"North East Zone",3:"North Eastern Hills Zone",4:"South Zone",5:"J&K Zone",6:"West Zone",7:"North Zone"}
 
     # plot_india_zone(colors,T)
     # plot_indi_zones(colors,T,data)
@@ -324,7 +326,70 @@ def part3():
                 uniq,ind,c=np.unique(M[i][j],return_inverse=True,return_counts=True)
                 data=np.append(data,np.array([(i,j,c[0],c[1],ind)],dtype=data.dtype))
 
-    zone_analysis(data)
+    # zone_analysis(data)
+    zyv(data)
+    # gpv(data)
+
+
+
+def gpv(data):
+    """
+    plot heat map of changing grid points
+    """
+
+    global T
+    t=np.zeros(121*121).reshape((121,121))
+    for i in data:
+        for j in range(len(i['year'])):
+            if j:
+                if i['year'][j] != i['year'][j-1]:
+                    t[i['X']][i['Y']]+=1
+
+    # plot_hm(t,"Gird point varaition in India")
+    for i in range(1,8,1):
+        plot_hm(t,"Gird point varaition in "+T[i],i)
+
+
+
+def zyv(data):
+    """
+    Visualize global/zonal change in grid points vs year
+    """
+
+    global T
+    D=[data]
+    Z=get_zone(data)
+
+    for i in range(7):
+        D.append(np.array([data[j] for j in Z[i]]))
+
+    for ind in range(len(D)):
+    
+        X=list(range(1952,2015,1))
+        Y=np.zeros(63)
+        d=D[ind]
+
+        for i in d:
+            for j in range(len(i['year'])):
+                if j:
+                    if i['year'][j] != i['year'][j-1]:
+                        Y[j-1]+=1
+
+        plt.rcParams.update({'font.size': 22})
+        plt.bar(X,Y,tick_label=X)
+        plt.xlabel("Year",fontsize=25)
+        plt.ylabel("Number of Points",fontsize=25)
+        plt.xticks(rotation=90)
+        figure = plt.gcf()
+        figure.set_size_inches(32, 18)
+
+        # if ind == 0:
+            # plt.title("Yearly change of grid point in India",fontsize=30)
+            # plt.savefig("images/india/"+"Yearly change of grid points in "+"India"+".png")
+        if ind:
+            plt.title("Yearly change of grid point in "+T[ind],fontsize=30)
+            plt.savefig("images/zonal_analysis/"+str(ind)+"/Yearly change of grid points in "+T[ind]+".png")
+        plt.close()
 
 
 
@@ -333,6 +398,7 @@ if __name__ == "__main__":
     PET,R=load_data()
     # (121, 121, 768) 64 years monthly data
 
+    T={1:"Central Zone",2:"North East Zone",3:"North Eastern Hills Zone",4:"South Zone",5:"J&K Zone",6:"West Zone",7:"North Zone"}
     # part1()
     # part2()
     part3()
