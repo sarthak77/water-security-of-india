8b2c3276d24f39bd2a910af1d1f7594e53603527
siddharthgaur <siddharthgaur8@gmail.com>
2020-06-29 11:32:55 +0530

    Updated code.py

--- code/code.py
+++ code/code.py
@@ -1,6 +1,6 @@
 from scipy.io import loadmat
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib.cm as cm
 import seaborn as sns
